3bf7aa949c981434024dc95a2d1b3f6cb2a1fd88
kcalled <kcalled@uteq.edu.ec>
2026-07-23 23:20:32 -0500

    C5: agregar unidad demostrable DemoPiscinas

---
+++ manual/ec/uteq/camaronera/piscinas/DemoPiscinas.java
@@ -0,0 +1,16 @@
+package ec.uteq.camaronera.piscinas;
+
+import java.sql.Date;
+
+public class DemoPiscinas {
+    public static void main(String[] args) {
+        Empleado empleado = new Empleado("0912345678", "Juan Pérez");
+        Camaronera camaronera = new Camaronera("AquaGest", "Quevedo");
+        Piscina piscina = new Piscina("P-01", 2.5, camaronera);
+        Date fechaSiembra = Date.valueOf("2026-01-15");
+        CicloCultivo ciclo = new CicloCultivo(fechaSiembra, 12, "activo", piscina);
+        Date fechaMuestreo = Date.valueOf("2026-02-01");
+        Muestreo muestreo = new Muestreo(fechaMuestreo, 8.5, empleado, ciclo);
+        System.out.println("Muestreos del ciclo: " + ciclo.getMuestreos().size());
+    }
+}
