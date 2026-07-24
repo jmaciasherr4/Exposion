4abe888a980049960f9280d3ca96120e47e5d036
kcalled <kcalled@uteq.edu.ec>
2026-07-24 01:31:52 -0500

    Modify Muestreo instantiation to add observations
    Updated Muestreo constructor to include observations.

--- manual/ec/uteq/camaronera/piscinas/DemoPiscinas.java
+++ manual/ec/uteq/camaronera/piscinas/DemoPiscinas.java
@@ -10,7 +10,7 @@ public class DemoPiscinas {
         Date fechaSiembra = Date.valueOf("2026-01-15");
         CicloCultivo ciclo = new CicloCultivo(fechaSiembra, 12, "activo", piscina);
         Date fechaMuestreo = Date.valueOf("2026-02-01");
-        Muestreo muestreo = new Muestreo(fechaMuestreo, 8.5, empleado, ciclo);
+        Muestreo muestreo = new Muestreo(fechaMuestreo, 8.5, "Sin observaciones", empleado, ciclo);
         System.out.println("Muestreos del ciclo: " + ciclo.getMuestreos().size());
     }
 }
